c0348aba47479022cbee9f8ba3dfb2bcdd73690a
Daniel Miller <danielm@vs-networks.com>
2013-05-02 13:32:21 -0400

    Fix symlink recursion in salt.utils.safe_walk

--- salt/utils/__init__.py
+++ salt/utils/__init__.py
@@ -891,3 +891,52 @@ def rm_rf(path):
             raise
 
     shutil.rmtree(path, onerror=_onerror)
+
+
+def safe_walk(top, topdown=True, onerror=None, followlinks=True, _seen=set()):
+    '''
+    A clone of the python os.walk function with some checks for recursive
+    symlinks. Unlike os.walk this follows symlinks by default.
+    '''
+    islink, join, isdir = os.path.islink, os.path.join, os.path.isdir
+
+    # We may not have read permission for top, in which case we can't
+    # get a list of the files the directory contains.  os.path.walk
+    # always suppressed the exception then, rather than blow up for a
+    # minor reason when (say) a thousand readable directories are still
+    # left to visit.  That logic is copied here.
+    try:
+        # Note that listdir and error are globals in this module due
+        # to earlier import-*.
+        names = os.listdir(top)
+    except os.error, err:
+        if onerror is not None:
+            onerror(err)
+        return
+
+    if followlinks:
+        stat = os.stat(top)
+        # st_ino is always 0 on some filesystems (FAT, NTFS); ignore them
+        if stat.st_ino != 0:
+            node = (stat.st_dev, stat.st_ino)
+            if node in _seen:
+                return
+            _seen.add(node)
+
+    dirs, nondirs = [], []
+    for name in names:
+        full_path = join(top, name)
+        if isdir(full_path):
+            dirs.append(name)
+        else:
+            nondirs.append(name)
+
+    if topdown:
+        yield top, dirs, nondirs
+    for name in dirs:
+        new_path = join(top, name)
+        if followlinks or not islink(new_path):
+            for x in safe_walk(new_path, topdown, onerror, followlinks, _seen):
+                yield x
+    if not topdown:
+        yield top, dirs, nondirs
